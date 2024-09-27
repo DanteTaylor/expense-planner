ba649f7e308d8e1db859728e8d9eaea57f83dfa5
Dante Taylor <135934495+DanteTaylor@users.noreply.github.com>
2024-09-27 07:29:10 -0400

    Update vite.config.js
    update name of project to reduce confusion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/expense-planner",
+  base: "/github-page-example",
   plugins: [react()],
 })
